6939e8a3b043fb8d7be3c3fffbe81e3a9c863172
JaredCJR <empire89090@gmail.com>
2016-01-08 16:19:15 +0800

    Modify linker script for STM32f429
    The flash size in original script is too small.

--- core/uvisor-config.h
+++ core/uvisor-config.h
@@ -17,7 +17,7 @@
 #ifndef  __UVISOR_CONFIG_H__
 
 #define UVISOR_MAGIC      0x2FE539A6
-#define UVISOR_FLASH_SIZE 0x6000
+#define UVISOR_FLASH_SIZE 0x6700
 #define UVISOR_SRAM_SIZE  0x2000
 
 #endif /*__UVISOR_CONFIG_H__*/
