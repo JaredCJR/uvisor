fdd206bdf94242dd2b49f4fc9f1119b8bead09ba
JaredCJR <empire89090@gmail.com>
2016-03-02 21:36:47 +0800

    Finish initStackPointers() in CrashCatcher
    FIXME:the unused variable error need to be fix by passing the warning.
    (in next commit)

--- CrashCatcher/src/CrashCatcher.c
+++ CrashCatcher/src/CrashCatcher.c
@@ -11,16 +11,53 @@
 uint32_t g_crashCatcherStack[CRASH_CATCHER_STACK_WORD_COUNT];
 
 
+/* Test harness will define this value on 64-bit machine to provide upper 32-bits of pointer addresses. */
+uint64_t g_crashCatcherTestBaseAddress;
+
+typedef struct
+{
+    const CrashCatcherExceptionRegisters* pExceptionRegisters;
+    const CrashCatcherStackedRegisters*   pSP;
+    uint32_t                              sp;
+    uint32_t                              flags;
+} Object;
+
+
+static uint32_t getAddressOfExceptionStack(const CrashCatcherExceptionRegisters* pExceptionRegisters)
+{
+    if (pExceptionRegisters->exceptionLR & LR_PSP)                                                                                               
+        return pExceptionRegisters->psp;
+    else
+        return pExceptionRegisters->msp;
+}
+
+static const void* uint32AddressToPointer(uint32_t address)
+{
+    if (sizeof(uint32_t*) == 8)
+        return (const void*)(unsigned long)((uint64_t)address | g_crashCatcherTestBaseAddress);                                                  
+    else
+        return (const void*)(unsigned long)address;
+}
+
+static Object initStackPointers(const CrashCatcherExceptionRegisters* pExceptionRegisters) 
+{
+    Object object;
+    object.pExceptionRegisters = pExceptionRegisters;
+    object.sp = getAddressOfExceptionStack(pExceptionRegisters);
+    object.pSP = uint32AddressToPointer(object.sp);
+    object.flags = 0;
+    return object;
+}
 
 
 void CrashCatcher_Entry(const CrashCatcherExceptionRegisters* pExceptionRegisters)
 {
     DPRINTF("\n-----*****CrashCatcher_Entry_start*****-----\n");
-    /*
+    
     Object object = initStackPointers(pExceptionRegisters);
-    advanceStackPointerToValueBeforeException(&object);
-    initFloatingPointFlag(&object);
-
+    //advanceStackPointerToValueBeforeException(&object);
+    //initFloatingPointFlag(&object);
+/*
     do
     {
         setStackSentinel();
