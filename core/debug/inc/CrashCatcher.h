a9d10563f2f0088f66d2c61c0b0f364caf6636e7
JaredCJR <empire89090@gmail.com>
2016-03-21 21:53:45 +0800

    Make CrashCatcher procedure more readable.

--- core/debug/inc/CrashCatcher.h
+++ core/debug/inc/CrashCatcher.h
@@ -2,7 +2,6 @@
 #ifndef __uvisor_CrashCatcher_H
 #define __uvisor_CrashCatcher_H
 
-#define ccADDR  (0x20000000 + 4 * 50);
 
 
 /* Definitions only required from C code. */
@@ -10,12 +9,14 @@
 
 #include <uvisor.h>
 
-
-uint32_t pCC_Storage_stack_lr  = (uint32_t)(0x20000000 + 4 * (50 - 1) );
-uint32_t pCC_Storage_stack_psp = (uint32_t)(0x20000000 + 4 * (50 - 11) );
-uint32_t pCC_Storage_stack_msp = (uint32_t)(0x20000000 + 4 * (50 - 10) );
-uint32_t pCC_Storage_autostack_top = (uint32_t)(0x20000000+ 4 * 50);
-uint32_t pCC_Storage_FaultStatus_top = (uint32_t)(0x20000000 + 4 * 95 );
+#define  pCC_Storage_Base    0x20000000
+#define  CC_WORD_SIZE        4
+uint32_t pCC_Storage_stack_bottom    = (uint32_t)(pCC_Storage_Base + CC_WORD_SIZE * (50 - 1) );
+uint32_t pCC_Storage_stack_lr        = (uint32_t)(pCC_Storage_Base + CC_WORD_SIZE * (50 - 1) );
+uint32_t pCC_Storage_stack_psp       = (uint32_t)(pCC_Storage_Base + CC_WORD_SIZE * (50 - 11) );
+uint32_t pCC_Storage_stack_msp       = (uint32_t)(pCC_Storage_Base + CC_WORD_SIZE * (50 - 10) );
+uint32_t pCC_Storage_autostack_top   = (uint32_t)(pCC_Storage_Base + CC_WORD_SIZE * 50 );
+uint32_t pCC_Storage_FaultStatus_top = (uint32_t)(pCC_Storage_Base + CC_WORD_SIZE * 95 );
 
 
 #endif // #if !__ASSEMBLER__
